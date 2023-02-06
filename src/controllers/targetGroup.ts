43e2373b0dd61d5ca3bd4d1a786bdf42470e20a2
LORDNR <ratchanons.work@gmail.com>
2023-02-06 16:38:23 +0700

    fix: response format. Edit validation

--- src/controllers/targetGroup.ts
+++ src/controllers/targetGroup.ts
@@ -1,62 +1,75 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime'
 import { Request, Response } from 'express'
-import { validationResult } from 'express-validator'
 
-import { prisma } from '../utils'
+import { ErrorResponse, SuccessResponse } from '../types/response'
+import { prisma, reqValidation } from '../utils'
+
+
 
 async function find(req: Request, res: Response) {
   try {
     const targetGroups = await prisma.targetGroup.findMany()
 
-    return res.status(200).json({ target_groups: targetGroups })
+    return res
+      .status(200)
+      .json({ status: 'success', data: targetGroups } as SuccessResponse)
   } catch (error) {
-    return res.status(400).json({ error })
+    return res
+      .status(400)
+      .json({ status: 'error', message: error } as ErrorResponse)
   }
 }
 
 async function findById(req: Request, res: Response) {
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() })
-  }
-
   try {
+    reqValidation(req)
+
     const { targetGroupId } = req.params
 
     const targetGroup = await prisma.targetGroup.findUnique({
       where: { id: targetGroupId },
     })
 
-    return res.status(200).json({ target_group: targetGroup })
+    return res
+      .status(200)
+      .json({ status: 'success', data: targetGroup } as SuccessResponse)
   } catch (error) {
-    return res.status(400).json({ errors })
+    // need to refactor later
+    if (error instanceof PrismaClientKnownRequestError) {
+      return res
+        .status(400)
+        .json({ status: 'error', message: error.name } as ErrorResponse)
+    }
+
+    return res
+      .status(400)
+      .json({ status: 'error', message: error } as ErrorResponse)
   }
 }
 
 async function create(req: Request, res: Response) {
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() })
-  }
-
   try {
+    reqValidation(req)
+
     const { name } = req.body
     const newTargetGroup = await prisma.targetGroup.create({
       data: { name },
     })
 
-    return res.status(201).json({ target_group: newTargetGroup })
+    return res
+      .status(200)
+      .json({ status: 'success', data: newTargetGroup } as SuccessResponse)
   } catch (error) {
-    return res.status(400).json({ errors })
+    return res
+      .status(400)
+      .json({ status: 'error', message: error } as ErrorResponse)
   }
 }
 
 async function update(req: Request, res: Response) {
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() })
-  }
-
   try {
+    reqValidation(req)
+
     const { targetGroupId } = req.params
     const { name } = req.body
 
@@ -65,14 +78,20 @@ async function update(req: Request, res: Response) {
       where: { id: targetGroupId },
     })
 
-    return res.status(200).json({ target_group: updatedTargetGroup })
+    return res
+      .status(200)
+      .json({ status: 'success', data: updatedTargetGroup } as SuccessResponse)
   } catch (error) {
-    return res.status(400).json({ error })
+    return res
+      .status(400)
+      .json({ status: 'error', message: error } as ErrorResponse)
   }
 }
 
 async function remove(req: Request, res: Response) {
   try {
+    reqValidation(req)
+
     const { targetGroupId } = req.params
 
     await prisma.targetGroup.delete({
@@ -81,7 +100,9 @@ async function remove(req: Request, res: Response) {
 
     return res.status(204).end()
   } catch (error) {
-    return res.status(400).json({ error })
+    return res
+      .status(400)
+      .json({ status: 'error', message: error } as ErrorResponse)
   }
 }
 
